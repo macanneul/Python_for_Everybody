be28e26d6705c8318db006c359128373ba2d81cc
Patrick Lee MacLeod <padraig@Patricks-MacBook-Pro.local>
2022-05-16 18:00:49 +0100

    first commit

---
+++ 07.01_Shout.py
@@ -0,0 +1,10 @@
+# Write a program to read through mbox-short.txt and print its contents (line by line) all in upper case.
+# Prompt the user to enter file name first.
+
+fname = input('Enter a file name: ')
+
+# Open the file
+
+# Print the first line
+
+# Lines will need rstrip() to get rid of /n
