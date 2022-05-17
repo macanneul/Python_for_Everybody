0e13f520186c04b1b594e1808e6a3500cdc27764
Patrick Lee MacLeod <padraig@Patricks-MacBook-Pro.local>
2022-05-17 14:05:22 +0100

    modified 07.01_shout.py with notations

--- 07.01_Shout.py
+++ 07.01_Shout.py
@@ -1,10 +1,19 @@
 # Write a program to read through mbox-short.txt and print its contents (line by line) all in upper case.
 # Prompt the user to enter file name first.
 
-fname = input('Enter a file name: ')
-
-# Open the file
-
-# Print the first line
-
-# Lines will need rstrip() to get rid of /n
+while True :
+    fname = input('Enter a file name: ')
+    # Deal with traceback due to file not found
+    try :
+        # Open the file
+        with open(fname, 'r') as fhandle :
+            for line in fhandle :
+                # Strip /n from end of line and print the line ALL CAPS
+                print(line.upper().rstrip())
+            break
+    except :
+        yn = input('File not found. Would you like to try again? (y/n): ')
+        if yn == 'y' :
+            continue
+        else :
+            break
