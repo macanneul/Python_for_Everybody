a8bc242f938b5d8829dc264cfe7b7c4bda8f46f6
Patrick Lee MacLeod <padraig@Patricks-MacBook-Pro.local>
2022-05-18 16:55:13 +0100

    updated comments

--- 08.05_Email_Client.py
+++ 08.05_Email_Client.py
@@ -1,6 +1,6 @@
-# Read through mbox-short.txt to find lines that start with "From"
+# Read through mbox-short.txt to find lines that start with 'From'
 # Split each line into words and print the sender's email address
-# Count and print, at the end, the number of "From" lines
+# Count and print, at the end, the number of 'From' lines
 
 # Open the file
 with open('mbox-short.txt', 'r') as fhandle :
@@ -8,7 +8,7 @@ with open('mbox-short.txt', 'r') as fhandle :
     for line in fhandle :
         # Strip /n at line's end and split
         wd = line.rstrip().split()
-        # To avoid blank lines and "IndexError: list index out of range"
+        # To avoid blank lines and IndexError: list index out of range
         # (Tried len(line)<1 and still got Traceback due to /n in blank lines)
         if len(line) < 2 or wd[0] != 'From' : continue
         print(wd[1])
