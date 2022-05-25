330c7a19e27d73d625fdc8bdbdf49eb3d082cb2c
macanneul <105060332+macanneul@users.noreply.github.com>
2022-05-25 11:58:07 +0100

    updated comments

--- 12.00_socket1.py
+++ 12.00_socket1.py
@@ -5,7 +5,9 @@
 import socket
 
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# connects through port 80
 mysock.connect(('data.pr4e.org', 80))
+# converts strings (in Unicode) to UTF-8
 cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
 mysock.send(cmd)
 
@@ -13,6 +15,7 @@ while True:
     data = mysock.recv(512)
     if len(data) < 1:
         break
+    # converts UTF-8 back to strings (in Unicode)
     print(data.decode(),end='')
 
 mysock.close()
