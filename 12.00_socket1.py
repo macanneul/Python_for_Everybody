c3aa63b8294757b9090b5c81e2e6626029982d1e
macanneul <105060332+macanneul@users.noreply.github.com>
2022-05-25 10:49:50 +0100

    first commit 12.00_socket1.py
    This is not my code but I copied it from http://www.py4e.com/code3/socket1.py to understand how sockets and simple web browsers work.

---
+++ 12.00_socket1.py
@@ -0,0 +1,17 @@
+# This is not my code but I copied it from http://www.py4e.com/code3/socket1.py
+# to understand how sockets and simple web browsers work.
+
+import socket
+
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('data.pr4e.org', 80))
+cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
+mysock.send(cmd)
+
+while True:
+    data = mysock.recv(512)
+    if len(data) < 1:
+        break
+    print(data.decode(),end='')
+
+mysock.close()
