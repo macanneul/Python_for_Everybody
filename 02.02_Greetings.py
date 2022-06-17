9255add18324bce67a848aabd189838dc78348c5
Patrick Lee MacLeod <padraig@Patricks-MacBook-Pro.local>
2022-06-17 15:04:53 +0100

    added f-string and cleaned up input

--- 02.02_Greetings.py
+++ 02.02_Greetings.py
@@ -1,4 +1,10 @@
 # Have user input his name and respond by greeting him
 
-n = input('Enter your name: ')
-print('Hallo,',n + '! ')
+# Prompt user to enter name
+# remove spaces from n (string) -- in case user falls asleep on the spacebar --
+# and capitalise the first letter(s)
+n = input('Enter your name: ').strip().title()
+# Note: capitalize() only changes the first letter of the string
+# whereas title() will capitalise first letter of the string 
+# AND first letter after each subsequent space
+print(f'Hallo, {n}!')
